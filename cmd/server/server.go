dc2aecb9b772fc77983fcd80270d0193e2feefe1
sifes <131526240+sifes@users.noreply.github.com>
2025-05-26 14:23:02 +0200

    Initial commit

---
+++ cmd/server/server.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/sifes/architecture-practice-4-template/httptools"
+	"github.com/sifes/architecture-practice-4-template/signal"
+)
+
+var port = flag.Int("port", 8080, "server port")
+
+const confResponseDelaySec = "CONF_RESPONSE_DELAY_SEC"
+const confHealthFailure = "CONF_HEALTH_FAILURE"
+
+func main() {
+	h := new(http.ServeMux)
+
+	h.HandleFunc("/health", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("content-type", "text/plain")
+		if failConfig := os.Getenv(confHealthFailure); failConfig == "true" {
+			rw.WriteHeader(http.StatusInternalServerError)
+			_, _ = rw.Write([]byte("FAILURE"))
+		} else {
+			rw.WriteHeader(http.StatusOK)
+			_, _ = rw.Write([]byte("OK"))
+		}
+	})
+
+	report := make(Report)
+
+	h.HandleFunc("/api/v1/some-data", func(rw http.ResponseWriter, r *http.Request) {
+		respDelayString := os.Getenv(confResponseDelaySec)
+		if delaySec, parseErr := strconv.Atoi(respDelayString); parseErr == nil && delaySec > 0 && delaySec < 300 {
+			time.Sleep(time.Duration(delaySec) * time.Second)
+		}
+
+		report.Process(r)
+
+		rw.Header().Set("content-type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(rw).Encode([]string{
+			"1", "2",
+		})
+	})
+
+	// additional links for testing different servers
+	h.HandleFunc("/api/v1/some-data-check1", func(rw http.ResponseWriter, r *http.Request) {
+		respDelayString := os.Getenv(confResponseDelaySec)
+		if delaySec, parseErr := strconv.Atoi(respDelayString); parseErr == nil && delaySec > 0 && delaySec < 300 {
+			time.Sleep(time.Duration(delaySec) * time.Second)
+		}
+
+		report.Process(r)
+
+		rw.Header().Set("content-type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(rw).Encode([]string{
+			"2", "3",
+		})
+	})
+
+	h.HandleFunc("/api/v1/some-data-check2", func(rw http.ResponseWriter, r *http.Request) {
+		respDelayString := os.Getenv(confResponseDelaySec)
+		if delaySec, parseErr := strconv.Atoi(respDelayString); parseErr == nil && delaySec > 0 && delaySec < 300 {
+			time.Sleep(time.Duration(delaySec) * time.Second)
+		}
+
+		report.Process(r)
+
+		rw.Header().Set("content-type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(rw).Encode([]string{
+			"3", "4",
+		})
+	})
+
+	h.HandleFunc("/api/v1/some-data-check3", func(rw http.ResponseWriter, r *http.Request) {
+		respDelayString := os.Getenv(confResponseDelaySec)
+		if delaySec, parseErr := strconv.Atoi(respDelayString); parseErr == nil && delaySec > 0 && delaySec < 300 {
+			time.Sleep(time.Duration(delaySec) * time.Second)
+		}
+
+		report.Process(r)
+
+		rw.Header().Set("content-type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(rw).Encode([]string{
+			"4", "5",
+		})
+	})
+
+	h.Handle("/report", report)
+
+	server := httptools.CreateServer(*port, h)
+	server.Start()
+	signal.WaitForTerminationSignal()
+}
